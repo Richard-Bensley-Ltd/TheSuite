8478072b70da6a6eb75e89e80c4ac4186c72af8e
Richard Bensley <2007952+rcbensley@users.noreply.github.com>
2024-03-06 15:58:03 +0000

    Update full-bkp.sh

--- full-bkp.sh
+++ full-bkp.sh
@@ -2,19 +2,55 @@
 
 set -e # exit on errors
 
-BACKUP_ROOT="${BACKUP_ROOT:=/mnt/data/backups}"
+#
+# Check server is a master and not a slave
+#
+
+# Slaves connected?
+SLAVES_CONNECTED=$(mariadb -NBe "select variable_value from information_schema.global_variables where variable_name='slaves_donnected'")
+if [[ $SLAVES_CONNECTED -gt 0 ]]
+then
+    echo "INFO SLaves Connected: ${SLAVES_CONNECTED}"
+else
+    echo "INFO No slaves Connected (${SLAVES_CONNECTED}) exiting"
+fi
+
+# Slave threads running?
+SLAVES_RUNNING=""
+for i in $(mariadb -E -Be "show slave status" |  awk '{$1=$1,print}' | egrep -i 'slave_sql_running:|slave_io_running' | cut -d" " -f 2)
+do
+if [[ "${i}" = "Yes" ]]
+    then
+        SLAVES_RUNNING="${i}"
+    fi
+done
+
+if [[ ${SLAVES_RUNNING} = "Yes" ]]
+then
+    echo "INFO Slave threads are running, exiting"
+    exit 0
+fi
+
+BACKUP_PREFIX="${BACKUP_PREFIX:=full-blk}"
+BACKUP_ROOT="${BACKUP_ROOT:=/glide/mysql/backup}"
+BACKUP_MOUNT="${BACKUP_MOUNT:=/mnt/db_backup_share/servicenow}"
 BACKUP_DATE="${BACKUP_DATE:=$(date +'%d%m%Y-%H%M%S')}"
-BACKUP_NAME="${BACKUP_FILE:=full-bkp-${BACKUP_DATE}}"
+BACKUP_NAME="${BACKUP_NAME:=${BACKUP_PREFIX}-${BACKUP_DATE}}"
 BACKUP_FILE="${BACKUP_FILE:=${BACKUP_ROOT}/${BACKUP_NAME}.gz}"
+BACKUP_MOUNT_FILE="${BACKUP_MOUNT_FILE:=${BACKUP_MOUNT}/${BACKUP_NAME}.gz}"
 BACKUP_MIN_SIZE=${BACKUP_MIN_SIZE:=100000000000} # 100GB
 BACKUP_DAYS_MAX=${BACKUP_DAYS_MAX:=7}
 
-mkdir -p /var/mariadb/backup
-
 if [[ ! -d ${BACKUP_ROOT} ]]
 then
-    echo "Creating backup root directory..."
-    mkdir -p ${BACKUP_ROOT}
+    echo "ERROR Backup root does exist ${BACKUP_ROOT}"
+    exit 1
+fi
+
+if [[ ! -d ${BACKUP_MOUNT} ]]
+then
+    echo "ERROR Backup mount does exist ${BACKUP_MOUNT}"
+    exit 1
 fi
 
 echo "Starting Backup to ${BACKUP_FILE}"
@@ -26,10 +62,18 @@ then
     ex1t 1
 fi
 
-if [[$(stat -c "%s" ${BACKUP_FILE}) -lt ${BACKUP_MIN_SIZE} ]]
+if [[ $(stat -c "%s" ${BACKUP_FILE}) -lt ${BACKUP_MIN_SIZE} ]]
 then
     echo "ERROR the backup file ${BACKUP_FILE} is less than ${BACKUP_MIN_SIZE} bytes"
     rm -f ${BACKUP_FILE}
+    exit 1
+fi
+
+echo "Moving ${BACKUP_FILE} to ${BACKUP_MOUNT_FILE}"
+rsync -r ${BACKUP_FILE} ${BACKUP_MOUNT_FILE} && rm -f ${BACKUP_FILE}
+if [[ ! -f ${BACKUP_MOUNT_FILE} ]]
+then
+    echo "ERROR the backup file could not be moved to ${BACKUP_MOUNT_FILE}"
     exit 1
 fi
 
