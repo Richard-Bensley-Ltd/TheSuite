968e62a52259267e894611234545c93b0c98505b
Richard Bensley <2007952+rcbensley@users.noreply.github.com>
2024-03-19 09:33:02 +0000

    Update sync-binlogs.sh

--- backup-restore/sync-binlogs.sh
+++ backup-restore/sync-binlogs.sh
@@ -21,12 +21,14 @@ then
     exit 1
 fi
 
+# Start
+echo -e "$(date +'%F_%H-%M-%S')\tINFO: Starting sync from ${SRC_DIR}/ to ${DESC_DIR}"
+
 # Trim any tailing slashes from both variables
 T_SRC_DIR=$(echo "$SRC_DIR" | sed 's:/*$::')
 T_DEST_DIR=$(echo "$DEST_DIR" | sed 's:/*$::')
 
 # Specifically add a trailing slash to the source
-echo -e "$(date +'%F_%H-%M-%S')\tINFO: Starting sync from ${T_SRC_DIR} to ${T_DESC_DIR}" && \
 rsync -a ${T_SRC_DIR}/ ${T_DEST_DIR} && \
-    echo -e "$(date +'%F_%H-%M-%S')\tINFO: Finished sync" \
-    && exit 0 || echo -e "$(date +'%F_%H-%M-%S')\tERROR: Sync failed from ${T_SRC_DIR} to ${T_DESC_DIR}" && exit 1
+    echo -e "$(date +'%F_%H-%M-%S')\tINFO: Finished sync" && \
+    exit 0 || echo -e "$(date +'%F_%H-%M-%S')\tERROR: Sync FAILED from ${T_SRC_DIR} to ${T_DESC_DIR}" && exit 1
