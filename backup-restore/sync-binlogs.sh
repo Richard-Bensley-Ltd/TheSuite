d0ffaf658d49f414f92ad44c4d6b4ee1504aeca5
Richard Bensley <2007952+rcbensley@users.noreply.github.com>
2024-03-19 09:27:24 +0000

    Update sync-binlogs.sh

--- backup-restore/sync-binlogs.sh
+++ backup-restore/sync-binlogs.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-print_usage() {
-    echo -e "$0 src_dir/ dest_dir"
-}
-
-
 if [[ $# -ne 2 ]]
 then
-    echo "Need a source and a destination directory"
-    print_usage
+    echo -e "$(date +'%F_%H-%M-%S')\tERROR: Need a source and a destination directory"
+    echo -e "USAGE: $0 src_dir/ dest_dir"
     exit 1
 fi
 
@@ -16,13 +11,13 @@ SRC_DIR="${1}"
 DEST_DIR="${2}"
 if [[ ! -d ${SRC_DIR} ]]
 then
-    echo "Cannot access source directory, ${SRC_DIR}"
+    echo -e "$(date +'%F_%H-%M-%S')\tERROR: Cannot access source directory, ${SRC_DIR}"
     exit 1
 fi
 
 if [[ ! -d ${DEST_DIR} ]]
 then
-    echo "Cannot access destination directory, ${DEST_DIR}"
+    echo -e "$(date +'%F_%H-%M-%S')\tERROR: Cannot access destination directory, ${DEST_DIR}"
     exit 1
 fi
 
@@ -31,5 +26,7 @@ T_SRC_DIR=$(echo "$SRC_DIR" | sed 's:/*$::')
 T_DEST_DIR=$(echo "$DEST_DIR" | sed 's:/*$::')
 
 # Specifically add a trailing slash to the source
-echo -e "2024-03-19_09-14-01\tINFO: Starting sync from ${T_SRC_DIR} to ${T_DESC_DIR}" && \
-rsync -av ${T_SRC_DIR}/ ${T_DEST_DIR}
+echo -e "$(date +'%F_%H-%M-%S')\tINFO: Starting sync from ${T_SRC_DIR} to ${T_DESC_DIR}" && \
+rsync -a ${T_SRC_DIR}/ ${T_DEST_DIR} && \
+    echo -e "$(date +'%F_%H-%M-%S')\tINFO: Finished sync" \
+    && exit 0 || echo -e "$(date +'%F_%H-%M-%S')\tERROR: Sync failed from ${T_SRC_DIR} to ${T_DESC_DIR}" && exit 1
