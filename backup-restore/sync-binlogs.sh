120a59871b18e4e9775bf57505c87dde775ffb8b
Richard Bensley <2007952+rcbensley@users.noreply.github.com>
2024-03-19 10:03:58 +0000

    Update sync-binlogs.sh

--- backup-restore/sync-binlogs.sh
+++ backup-restore/sync-binlogs.sh
@@ -21,7 +21,6 @@ then
     exit 1
 fi
 
-# Start
 # Trim any tailing slashes from both variables
 # A trailing slash is added to the source when rsync runs.
 T_SRC_DIR=$(echo "$SRC_DIR" | sed 's:/*$::')
@@ -29,9 +28,21 @@ T_DEST_DIR=$(echo "$DEST_DIR" | sed 's:/*$::')
 
 # Flush the binary logs so the current log file is closed and a new file is opened
 echo -e "$(date +'%F_%H-%M-%S')\tINFO: Flushing binary logs"
-mariadb -NBe "FLUSH BINARY LOGS" || echo -e "$(date +'%F_%H-%M-%S')\tERROR: Failed to flush binary logs!" && exit 1
+mariadb -NBe "FLUSH BINARY LOGS"
+if [[ $? -ne 0 ]]
+then
+    echo -e "$(date +'%F_%H-%M-%S')\tERROR: Failed to flush binary logs!"
+    exit 1
+fi
 
+# Start the sync
 echo -e "$(date +'%F_%H-%M-%S')\tINFO: Starting sync from ${SRC_DIR}/ to ${DEST_DIR}"
-rsync -a ${T_SRC_DIR}/ ${T_DEST_DIR} && \
-    echo -e "$(date +'%F_%H-%M-%S')\tINFO: Finished sync" && \
-    exit 0 || echo -e "$(date +'%F_%H-%M-%S')\tERROR: Sync FAILED from ${T_SRC_DIR} to ${T_DESC_DIR}" && exit 1
+rsync -a ${T_SRC_DIR}/ ${T_DEST_DIR}
+if [[ $? -ne 0 ]]
+then
+    echo -e "$(date +'%F_%H-%M-%S')\tERROR: Sync FAILED from ${T_SRC_DIR} to ${T_DESC_DIR}"
+    exit 1
+fi
+
+echo -e "$(date +'%F_%H-%M-%S')\tINFO: Finished sync" && \
+exit 0
